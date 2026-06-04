cf7b842188158c00c1f679cb2d4cf2b5239f4adb
Abhishek Anand <abhidotanand+github@gmail.com>
2026-06-04 18:20:31 +0530

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.go
@@ -0,0 +1,48 @@
+import (
+    "slices"
+)
+
+type MyHashSet struct {
+	Data []int
+}
+
+func Constructor() MyHashSet {
+	return MyHashSet{Data: []int{}}
+}
+
+func (this *MyHashSet) Add(key int) {
+	if !slices.Contains(this.Data, key) {
+		this.Data = append(this.Data, key)
+	}
+	return
+}
+
+func (this *MyHashSet) Remove(key int) {
+	var n int = len(this.Data)
+	var ind int = -1
+	for i := 0; i < n; i++ {
+		if this.Data[i] == key {
+			ind = i
+			break
+		}
+	}
+    if ind != -1{
+        slices.Delete(this.Data, ind, ind+1)
+    }
+	return
+}
+
+func (this *MyHashSet) Contains(key int) bool {
+	if slices.Contains(this.Data, key) {
+		return true
+	}
+	return false
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * param_3 := obj.Contains(key);
+ */
