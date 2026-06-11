4baba2a48624478ad6705ed1a634539a090a6ecf
Abhishek Anand <abhidotanand+github@gmail.com>
2026-06-11 18:10:17 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.go
@@ -0,0 +1,34 @@
+func isPalindrome(s string) bool {
+	var n int = len(s)
+	var front, rear int = 0, n-1
+
+	for front < rear {
+		for !isAlphabet(byte(s[front])) {
+			front++
+			if front >= n{
+				break
+			}
+		}
+
+		for !isAlphabet(byte(s[rear])) {
+			rear--
+			if rear < 0 {
+				break
+			}
+		}
+
+		if front >= rear {
+			break
+		}
+		if strings.ToLower((string(s[front]))) != strings.ToLower((string(s[rear]))) {
+			return false
+		}
+		front++
+		rear--
+	}
+	return true
+}
+
+func isAlphabet(i byte) bool {
+	return (i >= 65 && i <= 90) || (i >= 97 && i <= 122) || (i >= 48 && i <= 57)
+}
