4c63761e3a6c3350ce5a934133c6cfa5d3854618
Abhishek Anand <abhidotanand+github@gmail.com>
2026-06-12 12:51:24 +0530

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.go
@@ -0,0 +1,23 @@
+func mergeAlternately(word1 string, word2 string) string {
+	var n1 int = len(word1)
+	var n2 int = len(word2)
+	var tmp1, tmp2 int
+	var ans string
+
+	for tmp1 < n1 && tmp2 < n2 {
+		ans += string(word1[tmp1]) + string(word2[tmp2])
+		tmp1++
+		tmp2++
+	}
+
+	if tmp1 == n1 {
+		for i := tmp2; i < n2; i++ {
+			ans += string(word2[i])
+		}
+	} else if tmp2 == n2 {
+		for i := tmp1; i < n1; i++ {
+			ans += string(word1[i])
+		}
+	}
+	return ans
+}
