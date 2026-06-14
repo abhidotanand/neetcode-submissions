c3db034cf896bc18fe0e782656f7f326d2365838
Abhishek Anand <abhidotanand+github@gmail.com>
2026-06-14 20:11:53 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.go
@@ -0,0 +1,19 @@
+import "slices"
+func numRescueBoats(people []int, limit int) int {
+	var n int = len(people)
+	var front, rear = 0, n-1
+	var ans int
+
+	slices.Sort(people)
+
+	for front <= rear {
+		if people[front] + people[rear] > limit {
+			rear--
+		} else {
+			front++
+			rear--
+		}
+		ans++
+	}
+	return ans
+}
