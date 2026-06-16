3882a972bf1b6e813023863440542f22a1e13a0d
Abhishek Anand <abhidotanand+github@gmail.com>
2026-06-16 12:37:23 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.go
@@ -0,0 +1,35 @@
+func isValid(s string) bool {
+    var n int = len(s)
+    if n == 1 {
+        return false
+    }
+    var ans []string
+	
+    for i := 0; i < n; i++ {
+        if string(s[i]) == "(" || string(s[i]) == "{" || string(s[i]) == "[" {
+            ans = append(ans, string(s[i]))
+        } else if string(s[i]) == ")" {
+            if ans[len(ans)-1] == "(" {
+                ans = ans[:len(ans)-1]
+            } else {
+                return false
+            }
+        } else if string(s[i]) == "}" {
+            if ans[len(ans)-1] == "{" {
+                ans = ans[:len(ans)-1]
+            } else {
+                return false
+            }
+        } else if string(s[i]) == "]" {
+            if ans[len(ans)-1] == "[" {
+                ans = ans[:len(ans)-1]
+            } else {
+                return false
+            }
+        }
+    }
+    if len(ans) == 0 {
+        return true
+    }
+    return false
+}
