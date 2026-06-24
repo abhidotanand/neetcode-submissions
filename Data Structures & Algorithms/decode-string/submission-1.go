acddbbd4b5b1376e581aa1f7b276fa85261b607d
Abhishek Anand <abhidotanand+github@gmail.com>
2026-06-24 22:43:01 +0530

    Add: decode-string - submission-1

---
+++ Data Structures & Algorithms/decode-string/submission-1.go
@@ -0,0 +1,34 @@
+func decodeString(s string) string {
+	var st_num []int
+	var st_chr []string
+	var ans string
+
+	for i := 0; i < len(s); i++ {
+		c := string(s[i])
+		if num, err := strconv.Atoi(c); err == nil {
+			st_num = append(st_num, num)
+		} else if c == "[" {
+			st_chr = append(st_chr, "")
+		} else if c == "]" {
+			tmp := strings.Repeat(st_chr[len(st_chr)-1], st_num[len(st_num)-1])
+			st_chr = st_chr[:len(st_chr)-1]
+			st_num = st_num[:len(st_num)-1]
+			if len(st_chr) == 0 {
+				ans += tmp
+			} else {
+				st_chr[len(st_chr)-1] += tmp
+			}
+		} else {
+			if len(st_num) == 0 {
+				st_num = append(st_num, 1)
+				st_chr = append(st_chr, c)
+			} else {
+				st_chr[len(st_chr)-1] += c
+			}
+		}
+	}
+	if len(st_chr) > 0 {
+		return ans + st_chr[0]
+	}
+	return ans
+}
