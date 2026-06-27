5629c7cd4af99d94c0180a2a45c55105c14d1eae
Abhishek Anand <abhidotanand+github@gmail.com>
2026-06-27 22:41:02 +0530

    Add: largest-rectangle-in-histogram - submission-2

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-2.go
@@ -0,0 +1,58 @@
+func largestRectangleArea(heights []int) int {
+	var n int = len(heights)
+	var stack []int = make([]int, 0)
+	var lmost []int = make([]int, n)
+	var rmost []int = make([]int, n)
+	var ans int
+
+	for i := 0; i < n; i++ {
+		if len(stack) == 0 {
+			stack = append(stack, i)
+			lmost[i] = -1
+		} else {
+			if heights[i] > heights[stack[len(stack)-1]] {
+				lmost[i] = stack[len(stack)-1]
+				stack = append(stack, i)
+			} else {
+				for len(stack) > 0 && heights[i] <= heights[stack[len(stack)-1]] {
+					stack = stack[:len(stack)-1]
+				}
+				if len(stack) > 0 {
+					lmost[i] = stack[len(stack)-1]
+				} else {
+					lmost[i] = -1
+				}
+				stack = append(stack, i)
+			}
+		}
+	}
+
+	stack = stack[:0]
+	for i := n-1; i >= 0; i-- {
+		if len(stack) == 0 {
+			stack = append(stack, i)
+			rmost[i] = n
+		} else {
+			if heights[i] > heights[stack[len(stack)-1]] {
+				rmost[i] = stack[len(stack)-1]
+				stack = append(stack, i)
+			} else {
+				for len(stack) > 0 && heights[i] <= heights[stack[len(stack)-1]] {
+					stack = stack[:len(stack)-1]
+				}
+				if len(stack) > 0 {
+					rmost[i] = stack[len(stack)-1]
+				} else {
+					rmost[i] = n
+				}
+				stack = append(stack, i)
+			}
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		ans = max(ans, heights[i]*(rmost[i]-i-1) + heights[i]*(i-lmost[i]-1) +  heights[i])
+	}
+
+	return ans
+}
