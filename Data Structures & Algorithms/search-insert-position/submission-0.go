6217246fb9e928a172af4a31e0e7e9774c23adeb
Abhishek Anand <abhidotanand+github@gmail.com>
2026-06-28 16:05:46 +0530

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.go
@@ -0,0 +1,17 @@
+func searchInsert(nums []int, target int) int {
+	var n int = len(nums)
+	var left, right int = 0, n-1
+	var mid int
+
+	for left <= right {
+		mid = (left + right)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return mid+1
+}
