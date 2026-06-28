02443108cc49f326274e32f3984e17b246fd5b19
Abhishek Anand <abhidotanand+github@gmail.com>
2026-06-28 14:43:02 +0530

    Add: 4sum - submission-2

---
+++ Data Structures & Algorithms/4sum/submission-2.go
@@ -0,0 +1,43 @@
+import "slices"
+func fourSum(nums []int, target int) [][]int {
+	var n int = len(nums)
+	var i, j, k, l int = 0, 1, 2, n-1
+	var ans [][]int = make([][]int, 0)
+
+	slices.Sort(nums)
+
+	for i < n {
+		for i > 0 && i < n && nums[i] == nums[i-1] {
+			i++
+		}
+		
+		j = i+1
+		for j < k {
+			k = j+1
+			l = n-1
+			for k < l {
+				if nums[i] + nums[j] + nums[k] + nums[l] == target{
+					ans = append(ans, []int{nums[i], nums[j], nums[k], nums[l]})
+					k++
+					l--
+					for k < l && nums[k] == nums[k-1] {
+						k++
+					}
+					for k < l && nums[l] == nums[l+1] {
+						l--
+					}
+				} else if nums[i] + nums[j] + nums[k] + nums[l] < target {
+					k++
+				} else {
+					l--
+				}
+			}
+			j++
+			for j < k && nums[j] == nums[j-1] {
+				j++
+			}
+		}
+		i++
+	}
+	return ans
+}
