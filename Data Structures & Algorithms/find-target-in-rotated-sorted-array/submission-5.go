a1e7a31a591fde4f4a55017dc01f1d5d254abfa8
Abhishek Anand <abhidotanand+github@gmail.com>
2026-07-03 00:36:03 +0530

    Add: find-target-in-rotated-sorted-array - submission-5

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-5.go
@@ -0,0 +1,58 @@
+func search(nums []int, target int) int {
+	var n int = len(nums)
+	var left, right int = 0, n-1
+
+	if n == 1 {
+		if target == nums[0] {
+			return 0
+		}
+		return -1
+	}
+
+	for left <= right {
+		mid := (left+right)/2
+
+		if nums[mid] == target {
+			return mid
+		} else if mid > 0 && nums[mid-1] == target {
+			return mid - 1
+		} else if mid < n-1 && nums[mid+1] == target {
+			return mid + 1
+		}
+
+		if mid == 0 || mid == n-1 {
+			if target == nums[0] {
+				return 0
+			} else if target == nums[1] {
+				return 1
+			} else if target == nums[n-1] {
+				return n-1
+			} else if target == nums[n-2] {
+				return n-2
+			} else {
+				return -1
+			}
+		}
+
+		if nums[left] <= nums[mid-1] && nums[mid+1] >= nums[right] {
+			if target <= nums[mid-1] && target >= nums[left] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else if nums[left] >= nums[mid-1] && nums[mid+1] <= nums[right] {
+			if target >= nums[mid+1] && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		} else {
+			if target >= nums[left] && target <= nums[mid-1] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		}
+	}
+	return -1
+}
