cd58bb52befd3c7b3dfe04b4c5b1dacaa3ed5fb9
Abhishek Anand <abhidotanand+github@gmail.com>
2026-07-04 13:04:45 +0530

    Add: search-in-rotated-sorted-array-ii - submission-3

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-3.go
@@ -0,0 +1,29 @@
+func search(nums []int, target int) bool {
+	var n int = len(nums)
+	var left, right int = 0, n-1
+
+	for left <= right {
+		mid := (left+right)/2
+
+		if nums[mid] == target {
+			return true
+		}
+
+		if nums[mid] > nums[left] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else if nums[mid] < nums[left] {
+			if target > nums[mid] && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		} else {
+			left++
+		}
+	}
+	return false
+}
