f417a526dfc1829ee409c3b13c4c3b40db02119e
Abhishek Anand <abhidotanand+github@gmail.com>
2026-07-05 18:25:45 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.go
@@ -0,0 +1,24 @@
+func containsNearbyDuplicate(nums []int, k int) bool {
+	var n int = len(nums)
+	var m map[int]struct{} = make(map[int]struct{})
+	var front, rear int
+
+	for rear <= k {
+		if _, ok := m[nums[rear]]; ok {
+			return true
+		}
+		m[nums[rear]] = struct{}{}
+		rear++
+	}
+
+	for rear < n {
+		delete(m, nums[front])
+		if _, ok := m[nums[rear]]; ok {
+			return true
+		}
+		m[nums[rear]] = struct{}{}
+		front++
+		rear++
+	}
+	return false
+}
