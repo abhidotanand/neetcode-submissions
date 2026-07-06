7e8320ff0e4f98bf28569bb55199007b0c451fbc
Abhishek Anand <abhidotanand+github@gmail.com>
2026-07-06 14:21:05 +0530

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.go
@@ -0,0 +1,29 @@
+func lengthOfLongestSubstring(s string) int {
+	var n int = len(s)
+	var front, rear int
+	var m map[byte]int = make(map[byte]int)
+	var ans int
+
+	for rear < n {
+		if _,ok := m[s[rear]]; !ok {
+			ans++
+			m[s[rear]] = rear
+		} else {
+			break
+		}
+		rear++
+	}
+
+	tmp := 0
+	for rear < n {
+		if _, ok := m[s[rear]]; !ok {
+			tmp = rear - front + 1
+		} else {
+			ans = max(ans, tmp)
+			front = m[s[rear]] + 1
+			m[s[rear]] = rear
+		}
+		rear++
+	}
+	return ans
+}
