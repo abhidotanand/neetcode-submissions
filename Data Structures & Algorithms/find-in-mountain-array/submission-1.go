34c5852761ae934e1e0571d8f5f472940ef7782e
Abhishek Anand <abhidotanand+github@gmail.com>
2026-07-11 22:16:23 +0530

    Add: find-in-mountain-array - submission-1

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-1.go
@@ -0,0 +1,68 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * type MountainArray struct {
+ * }
+ *
+ * func (this *MountainArray) get(index int) int {}
+ * func (this *MountainArray) length() int {}
+ */
+
+func findInMountainArray(target int, mountainArr *MountainArray) int {
+    var n int = mountainArr.length()
+	var left, right = 1, n-2
+	var tip int
+
+	for left <= right {
+		mid := (left+right)/2
+
+		midEle := mountainArr.get(mid)
+		prevEle := mountainArr.get(mid-1)
+		nextEle := mountainArr.get(mid+1)
+		
+		if midEle > prevEle && midEle > nextEle {
+			tip = mid
+			break
+		} else if midEle < prevEle && midEle > nextEle {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	left = 0
+	right = tip
+
+	for left <= right {
+		mid := (left+right)/2
+
+		midEle := mountainArr.get(mid)
+
+		if midEle == target {
+			return mid
+		} else if midEle > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	left = tip + 1
+	right = n-1
+
+	for left <= right {
+		mid := (left+right)/2
+
+		midEle := mountainArr.get(mid)
+
+		if midEle == target {
+			return mid
+		} else if midEle > target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return -1
+}
