ea82a270ff62e02b595f1de1bf84f11be72e9994
Abhishek Anand <abhidotanand+github@gmail.com>
2026-07-13 18:13:38 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.go
@@ -0,0 +1,34 @@
+import "slices"
+func checkInclusion(s1 string, s2 string) bool {
+	var n1 int = len(s1)
+	var n2 int = len(s2)
+
+	if n1 > n2 {
+		return false
+	}
+
+	var freq_arr []int = make([]int, 26)
+	var window []int = make([]int, 26)
+
+	for i := range s1 {
+		freq_arr[s1[i] - 97]++
+	}
+
+	for i := 0; i < n1; i++ {
+		window[s2[i] - 97]++
+	}
+
+	if slices.Equal(freq_arr, window) {
+		return true
+	}
+
+	front, rear := 0, n1
+	for ;rear < n2; front, rear = front+1, rear+1 {
+		window[s2[rear] - 97]++
+		window[s2[front] - 97]--
+		if slices.Equal(freq_arr, window) {
+			return true
+		}
+	}
+	return false
+}
