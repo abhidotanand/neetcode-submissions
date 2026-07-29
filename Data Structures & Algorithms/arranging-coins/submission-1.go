fcedc2dfcaf21ffe9b2a850d6b58f786feede5c6
Abhishek Anand <abhidotanand+github@gmail.com>
2026-07-29 21:51:51 +0530

    Add: arranging-coins - submission-1

---
+++ Data Structures & Algorithms/arranging-coins/submission-1.go
@@ -0,0 +1,22 @@
+func arrangeCoins(n int) int {
+	var l, r int = 1, n
+	var mid int
+
+	for l <= r {
+		mid = (l + r)/2
+		summ := mid*(mid+1)/2
+
+		if summ == n {
+			return mid
+		} else if summ > n {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	
+	if mid*(mid+1)/2 > n {
+		return mid - 1
+	}
+	return mid
+}
