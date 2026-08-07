2bc86394a491864d99c17760e49d812c97860ab9
Abhishek Anand <abhidotanand+github@gmail.com>
2026-08-07 09:32:27 +0530

    Add: number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold - submission-1

---
+++ Data Structures & Algorithms/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/submission-1.go
@@ -0,0 +1,26 @@
+func numOfSubarrays(arr []int, k int, threshold int) int {
+	var n int = len(arr)
+	var front, rear int
+	var sum int
+	var ans int
+
+	for rear < k {
+		sum += arr[rear]
+		rear++
+	}
+
+	if sum/k >= threshold {
+		ans++
+	}
+
+	for rear < n {
+		sum += arr[rear]
+		sum -= arr[front]
+		if sum/k >= threshold {
+			ans++
+		}
+		front++
+		rear++
+	}
+	return ans
+}
