5d205690fb119d1d633c366c78b593ca1aa359c7
Abhishek Anand <abhidotanand+github@gmail.com>
2026-08-15 14:24:51 +0530

    Add: check-if-array-is-sorted-and-rotated - submission-1

---
+++ Data Structures & Algorithms/check-if-array-is-sorted-and-rotated/submission-1.go
@@ -0,0 +1,29 @@
+func check(nums []int) bool {
+    var n int = len(nums)
+	var max_idx int = n - 1
+
+	for i := 0; i < n-1; i++ {
+		if nums[i] > nums[i+1] {
+			max_idx = i
+		}
+	}
+
+	for i := 0; i < max_idx; i++ {
+		if nums[i] > nums[i+1] {
+			return false
+		}
+	}
+
+	for i := max_idx+1; i < n-1; i++ {
+		if nums[i] > nums[i+1] {
+			return false
+		}
+	}
+
+	if max_idx == n-1 {
+		return true
+	} else if nums[0] >= nums[n-1] {
+		return true
+	}
+	return false
+}
