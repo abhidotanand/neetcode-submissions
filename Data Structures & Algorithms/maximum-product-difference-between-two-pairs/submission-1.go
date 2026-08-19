b413c1a12faf205be5c1517b4f924c941951e145
Abhishek Anand <abhidotanand+github@gmail.com>
2026-08-19 21:54:39 +0530

    Add: maximum-product-difference-between-two-pairs - submission-1

---
+++ Data Structures & Algorithms/maximum-product-difference-between-two-pairs/submission-1.go
@@ -0,0 +1,38 @@
+import "slices"
+func maxProductDifference(nums []int) int {
+	var n int = len(nums)
+    var mini1 int = slices.Min(nums)
+	var maxi1 int = slices.Max(nums)
+	var mini2, maxi2 int = 10001, 0
+	var minicnt, maxicnt int
+
+	for i := 0; i < n; i++ {
+		if nums[i] > mini1 && nums[i] < mini2 {
+			mini2 = nums[i]
+		}
+
+		if nums[i] < maxi1 && nums[i] > maxi2 {
+			maxi2 = nums[i]
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		if nums[i] == mini1 {
+			minicnt++
+		}
+
+		if nums[i] == maxi1 {
+			maxicnt++
+		} 
+	}
+
+	if minicnt > 1 {
+		mini2 = mini1
+	}
+
+	if maxicnt > 1 {
+		maxi2 = maxi1
+	}
+
+	return maxi2*maxi1 - mini2*mini1
+}
