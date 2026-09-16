88db83d7b1955a084bb1fb3dca2bedf01e822db9
Abhishek Anand <abhidotanand+github@gmail.com>
2026-09-17 00:30:59 +0530

    Add: is-subsequence - submission-2

---
+++ Data Structures & Algorithms/is-subsequence/submission-2.go
@@ -0,0 +1,18 @@
+func isSubsequence(s string, t string) bool {
+	var n, m int = len(s), len(t)
+	var i, j int
+
+	if n == 0 {
+		return true
+	}
+
+	for j = 0; j < m; j++ {
+		if t[j] == s[i] {
+			i++
+		}
+		if i == n {
+			return true
+		}
+	}
+	return false
+}
